ad5027bce5a299333995861e80441b2871d1a40f
IllEgo <robertmicahperalta@gmail.com>
2025-09-20 20:55:06 -1000

    Fixed a few issues with the audio recording function.

--- app/src/main/java/com/e3hi/geodrop/ui/AudioRecorderActivity.kt
+++ app/src/main/java/com/e3hi/geodrop/ui/AudioRecorderActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -210,6 +211,7 @@ class AudioRecorderActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun AudioRecorderRoute(
     onStartRecording: ((onError: (String) -> Unit) -> Boolean),
