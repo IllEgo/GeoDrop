a775835b8094fc6afe049cabf7cbbf77c01d42cc
IllEgo <robertmicahperalta@gmail.com>
2025-09-23 16:29:40 -1000

    Added:
    A business user role
    drop type for different types of business like restaurant for coupons or tour stops for tour guides
    A redemption flow to prevent abuse
    A dashboard for businesses to see results
    
    Errors

--- app/src/main/java/com/e3hi/geodrop/ui/DropDetailActivity.kt
+++ app/src/main/java/com/e3hi/geodrop/ui/DropDetailActivity.kt
@@ -68,6 +68,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.core.app.NotificationManagerCompat
