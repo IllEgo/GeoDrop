eb8772399f81255d529e09683399a875d82d2cc3
Robert Peralta <robertmicahperalta@gmail.com>
2025-10-24 17:08:46 -1000

    Added an iOS 17 availability check before applying textScaleEffect so older OS versions continue using the compact style without the unavailable modifier.

--- ios/GeoDropIOS/Utilities/GeoDropCompactStyle.swift
+++ ios/GeoDropIOS/Utilities/GeoDropCompactStyle.swift
@@ -2,10 +2,16 @@ import SwiftUI
 
 struct GeoDropCompactStyle: ViewModifier {
     func body(content: Content) -> some View {
-        content
-            .dynamicTypeSize(.xSmall ... .medium)
-            .textScaleEffect(0.78, anchor: .topLeading)
-            .environment(\.imageScale, .small)
+        if #available(iOS 17.0, *) {
+            content
+                .dynamicTypeSize(.xSmall ... .medium)
+                .textScaleEffect(0.78, anchor: .topLeading)
+                .environment(\.imageScale, .small)
+        } else {
+            content
+                .dynamicTypeSize(.xSmall ... .medium)
+                .environment(\.imageScale, .small)
+        }
     }
 }
 
