07f2ce8c1606c8a906fcea64d2da87ef4533dd41
Robert Peralta <robertmicahperalta@gmail.com>
2025-10-24 17:14:29 -1000

    Removed the textScaleEffect modifier from GeoDropCompactStyle so the view modifier remains compatible with SDKs that do not expose that API.

--- ios/GeoDropIOS/Utilities/GeoDropCompactStyle.swift
+++ ios/GeoDropIOS/Utilities/GeoDropCompactStyle.swift
@@ -3,16 +3,9 @@ import SwiftUI
 struct GeoDropCompactStyle: ViewModifier {
     func body(content: Content) -> some View {
         if #available(iOS 17.0, *) {
-#if swift(>=5.9)
-            content
-                .dynamicTypeSize(.xSmall ... .medium)
-                .textScaleEffect(0.78, anchor: .topLeading)
-                .environment(\.imageScale, .small)
-#else
             content
                 .dynamicTypeSize(.xSmall ... .medium)
                 .environment(\.imageScale, .small)
-#endif
         } else {
             content
                 .dynamicTypeSize(.xSmall ... .medium)
